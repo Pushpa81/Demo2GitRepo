0b5fe35bf478f9cc9a69ef06954f1510d7336e8c
TRACLabs <pushpaparam02@gmail.com>
2017-10-25 14:30:48 -0500

    Demo2 project has been pushed local repo

---
+++ Demo2/src/TestPkg/TestClass.java
@@ -0,0 +1,11 @@
+package TestPkg;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("this is second test");
+	}
+
+}
